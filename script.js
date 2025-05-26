9df0dc595a42608d7d93d0e5911137a773fc4d0f
nozzaws <nozzaws@gmail.com>
2025-05-26 23:55:43 +0200

    logic things and spawner

--- script.js
+++ script.js
@@ -1,147 +1,144 @@
-/** @type {HTMLCanvasElement} */
-const canvas = document.getElementById('myCanvas')
-const ctx = canvas.getContext('2d')
-CANVAS_WIDTH = canvas.width = 800;
-CANVAS_HEIGHT = canvas.height = 600;
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 600;
+const offsetL = canvas.offsetLeft;
+const offsetT = canvas.offsetTop;
 
-
-let centerCanvasPosition = function(canvas){
-  canvas.style.left = window.innerWidth / 2 - canvas.width / 2 + 'px';
-  canvas.style.top =window.innerHeight / 2 - canvas.height / 2 + 'px';
-};
- 
-ctx.fillStyle='black';
-ctx.fillRect(0,0,canvas.width,canvas.height);
- 
-// center canvas for starters
-centerCanvasPosition(canvas);
-// and on every resize
-window.addEventListener('resize',function(){
-    centerCanvasPosition(canvas);
-});
-
-
-let offsetL = canvas.offsetLeft
-let offsetT = canvas.offsetTop
-
-let enemyArray = [];
-const amountOfEnemies = 7;
-let newAr = []
 let score = 0;
+let enemyArray = [];
+
+const amountOfEnemies = 3;
+let level = amountOfEnemies
 
 
-class Enemy{
-    constructor(IDX){
-        this.idx = IDX
-        this.text = "Text"
-        this.h = 100;
-        this.w = this.h;
-        this.x = Math.random() * (canvas.width - this.w);
-        this.y = Math.random() * (canvas.height - this.h);
-        this.speedx = Math.random() * 2 - 1;
-        this.speedy = Math.random() * 2 - 1;
-        this.color = `rgb(${Math.random()}, ${Math.random() * 255}, ${Math.random()*125})`
-        this.id = Math.floor(Math.random()*1000)
-        this.hit = false
-        
-    }
+class Enemy {
+  constructor(idx) {
+    this.idx = idx;
+    this.w = 50;
+    this.h = this.w;
+    this.bounced = 0;
+    this.bouncePenalty = false
 
-    update(delta){
-        this.x-= Math.sin(this.speedx * delta/50)
-        if(this.x + this.w < 0 + this.w || this.x + this.w > canvas.width) this.speedx *= -1
-        this.y-= this.speedy
-        if(this.y + this.h < 0 + this.h || this.y + this.h > canvas.height) this.speedy *= -1
-    }
+    // Initial position
+    this.x = Math.random() * (canvas.width - this.w)
+    this.y = canvas.height - this.h
+
+    // Movement
+    this.speedX = (Math.random() - 0.5) * 400;
+    this.speedY = 500;
+    this.gravity = 1000;
+    this.velocityY = (Math.random() * -400) - 600;
+    this.color = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 125)})`;
+    this.hit = false;
+  }
+
+  update(deltaTime) {
     
-    draw(){
-        ctx.fillStyle = this.color
-        ctx.fillRect(this.x, this.y, this.w, this.h);
+    //make it heavy yo
+    this.velocityY += this.gravity * deltaTime;
+    
+    // Update position based on velocity/gravity
+    this.x += this.speedX * deltaTime;
+    this.y += this.velocityY * deltaTime;
 
-
-        //text in box rn
-        ctx.font = "50px Arial";
-        ctx.fillStyle = "Black"
-        ctx.fillText("Hej", this.x+this.w/2, this.y+this.h/2+3)
-        ctx.fillStyle = "White"
-        ctx.fillText("Hej", this.x+this.w/2, this.y+this.h/2)
-        
+    if(this.x + this.w < 0 + this.w || this.x + this.w > canvas.width) this.speedX *= -1
+    if(this.y + this.h < 0 + this.h || this.y + this.h > canvas.height){
+      this.speedY *= -1
+      this.bounced++
+      }
+    if(this.bounced > 2){
+      this.bouncePenalty = true
     }
 
+    // canvas borders and bounce
+    if (this.y < 0) {
+      this.y = 0;
+      this.velocityY *= -0.95; // Dampen the bounce
+    } else if (this.y + this.h > canvas.height) {
+      this.y = canvas.height - this.h;
+      this.velocityY *= -0.95;
+    }
+  }
+
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+    ctx.font = "20px Arial";
+    ctx.fillStyle = "Black";
+    ctx.fillText(this.bounced, this.x + this.w / 2, this.y + this.h / 2 + 3);
+    ctx.fillStyle = "White";
+    // ctx.fillText("Hej", this.x + this.w / 2, this.y + this.h / 2);
+  }
 }
-
-function updateScore(){
-    ctx.font = "50px Arial";
-    ctx.fillText(`${score}`,10,80)
+// Initialize enemies
+for (let i = 0; i < amountOfEnemies; i++) {
+  enemyArray.push(new Enemy(i));
 }
-
-
-for(let i = 0; i<amountOfEnemies;i++){
+function spawn(num) {
+  for (let i = 0; i < num; i++) {
     enemyArray.push(new Enemy(i));
+  }
 }
 
-
-function spawn(num){
-    // console.log('spawning...')
-    for(let i =0 ; i < num; i++){
-        enemyArray.push(new Enemy(i));
-    }
+function updateScore() {
+  ctx.font = "30px Arial";
+  ctx.fillStyle = "white";
+  ctx.fillText(`Score: ${score}`, 10, 40);
 }
+
 
 canvas.addEventListener('click', (e) => {
-    const clickX = e.clientX - offsetL
-    const clickY = e.clientY - offsetT
+  const clickX = e.clientX - offsetL;
+  const clickY = e.clientY - offsetT;
 
-    for(let i = 0; i<enemyArray.length;i++){
-        let enemyX = enemyArray[i].x
-        let enemyY = enemyArray[i].y
-        if(clickX >= enemyX && clickX < enemyX + enemyArray[i].w && clickY >= enemyY && clickY < enemyY + enemyArray[i].h ) {
-        enemyArray[i].hit = true
-        score++
-        break;
-        // console.log('You hit enemy : ' + enemyArray[i].idx)
-         
-    
-        }
+  for (let i = 0; i < enemyArray.length; i++) {
+    const enemy = enemyArray[i];
+      if (clickX >= enemy.x &&
+        clickX < enemy.x + enemy.w &&
+        clickY >= enemy.y &&
+        clickY < enemy.y + enemy.h)
+      {
+        enemy.hit = true;
+        score++;
+      }
+     
     }
-
-
-    // enemyArray.forEach(enemy => {
-    //     if(clickX >= enemy.x && clickX < enemy.x + enemy.w &&
-    //     clickY >= enemy.y && clickY < enemy.y + enemy.h ) {
-    //         enemy.hit = true
-    //         timeSinceLastClick = 0;
-    //         afterClick = enemyArray.length
-    //     }
-
-    // });
-
-    
-
 });
 
+function checkPenalty(arr){
+  for(let i = 0; i<enemyArray.length;i++){
+  if(enemyArray[i].bounced){
+        score -= enemyArray[i].bounced
+      }
+    }
+  }
 
+let lastTime = 0;
 
+function animate(time) {
 
-const reload = () => {
-    spawn(5);
-}
+  if(enemyArray.length === 0){
+    spawn(level + 1)
+    level++
+  }
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  const deltaTime = time - lastTime;
+  lastTime = time;
 
+  // remove amount of bounces left if an object exists from score
+  // soooooo if you have killed two on level 1, but miss one you get +2 and -2
+  enemyArray = enemyArray.filter((object) => !object.hit && !object.bouncePenalty)
+  
 
-function animate(timeStamp) {
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-
-    enemyArray = enemyArray.filter((object) => !object.hit)
-
+  enemyArray.forEach(enemy => {
+    enemy.update(deltaTime / 1000);
+    enemy.draw();
     
-    enemyArray.forEach(enemy => {
-        enemy.update(timeStamp);
-        enemy.draw();
-    });
-
-    if(enemyArray.length === 0) reload();
-    updateScore()
-    requestAnimationFrame(animate);
-
+  });
+    
+  updateScore();
+  requestAnimationFrame(animate);
 }
-animate(0)
 
+animate(0)
